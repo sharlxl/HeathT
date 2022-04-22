268ebbfcf36df14e0543522c086c49ddc27f567a
sharlxl <sharleneee@gmail.com>
2022-04-22 13:14:51 +0800

    initialise react app

---
+++ healtht/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>menu, Logo, name, logout</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ healtht/src/pages/SignUpPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const signupPage = () => {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  );
+};
+
+export default signupPage;
