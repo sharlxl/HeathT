43b52e5247db523f9c418c67ef928e02ea2f2d44
sharlxl <sharleneee@gmail.com>
2022-04-22 15:42:40 +0800

    changed React ver to 17.0.2

--- healtht/src/App.js
+++ healtht/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/main" element={<MainPage />} />
+      <Route path="/new" element={<SignUpPage />} />
+    </Routes>
   );
 }
 
